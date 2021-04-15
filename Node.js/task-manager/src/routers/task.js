186fb258e37526a594f1cb2796c7612b9deb82f4
phertfelder <52052598+phertfelder@users.noreply.github.com>
2021-04-14 19:32:06 -0500

    task router

---
+++ Node.js/task-manager/src/routers/task.js
@@ -0,0 +1,128 @@
+const express = require("express")
+const Task = require("../models/task")
+const User = require("../models/user")
+const auth = require("../middleware/auth")
+const { request } = require("express")
+
+const router = new express.Router()
+//Create new task JSON
+router.post("/tasks", auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  })
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+  // task
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(task)
+  //   })
+  //   .catch((e) => {
+  //     res.status(400).send(e)
+  //   })
+})
+//Get all tasks JSON
+//GET /tasks?completed=false
+//limit skip
+//GET/tasks?limit=10&skip=0
+//GET/tasks?sortBy=CreatedAt_desc
+router.get("/tasks", auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true"
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":")
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1
+  }
+  try {
+    //const tasks = await Task.find({ owner: req.user._id })
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate()
+
+    res.send(req.user.tasks)
+
+    //res.send(tasks)
+  } catch (e) {
+    res.status(500).send()
+  }
+
+  // Task.find({})
+  //   .then((task) => {
+  //     res.send(task)
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send()
+  //   })
+})
+
+//Get Task by ID JSON
+router.get("/tasks/:id", auth, async (req, res) => {
+  const _id = req.params.id
+  try {
+    const task = await Task.findOne({ _id, owner: req.user._id })
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+//Update a task by id of mongoose
+// JSON Data
+router.patch("/tasks/:id", auth, async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ["description", "completed"]
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  )
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" })
+  }
+  try {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+
+    if (!task) {
+      return res.status(404).send()
+    }
+    updates.forEach((update) => (task[update] = req.body[update]))
+    await task.save()
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+router.delete("/tasks/:id", auth, async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    })
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+module.exports = router
